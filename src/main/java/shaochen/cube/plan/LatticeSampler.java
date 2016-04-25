dff095605c18fcb1581f3ea35561424c3fe2c5af
shishaochen <shishaochen_ha@sina.com>
2016-04-25 21:10:10 +0800

    整合LatticeSampler和LatticeEvaluator。

--- src/main/java/shaochen/cube/plan/LatticeSampler.java
+++ src/main/java/shaochen/cube/plan/LatticeSampler.java
@@ -1,12 +1,20 @@
 package shaochen.cube.plan;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -27,7 +35,7 @@ import shaochen.cube.util.MetaInfo;
  *
  */
 public class LatticeSampler {
-	
+
 	/**
 	 * 在抽样给定数据集上估算出搜索格上各聚集操作的结果大小。
 	 * @param context Spark程序上下文。
@@ -36,12 +44,21 @@ public class LatticeSampler {
 	 * @return 不同聚集任务的结果大小。
 	 */
 	public static Map<Integer, Long> estimateCuboidSize(JavaSparkContext context, String inputPath, int dimensionCount) {
+		double fraction = LatticeSampler.calculateSampleFraction(context, inputPath);
+		return LatticeSampler.estimateCuboidSize(context, inputPath, fraction, dimensionCount);
+	}
+	
+	private static Map<Integer, Long> estimateCuboidSize(JavaSparkContext context, String inputPath, double fraction, int dimensionCount) {
 		Map<Integer, Long> map = new HashMap<Integer, Long>();
 		
 		//配置采样参数
-		double fraction = LatticeSampler.calculateSampleFraction(context, inputPath);
 		int parallelism = context.getConf().getInt("spark.default.parallelism", 20);
-		JavaRDD<String> lines = context.textFile(inputPath, parallelism).sample(false, fraction, new Random().nextInt());
+		JavaRDD<String> lines = null;
+		if (Math.abs(1.0 - fraction) < 1e-6) { //全量加载
+			lines = context.textFile(inputPath, parallelism);
+		} else {
+			lines = context.textFile(inputPath, parallelism).sample(false, fraction, new Random().nextInt());
+		}
 		
 		//解析数据并缓存
 		final Broadcast<Integer> bWidth = context.broadcast(dimensionCount);
@@ -135,6 +152,51 @@ public class LatticeSampler {
 		Path p = new Path(hdfsPath);
 		FileSystem hdfs = p.getFileSystem(new Configuration());
 		return 1.0 * hdfs.getContentSummary(p).getLength() / MetaInfo.MB_TO_BYTE;
+	}
+
+	private static Options createCmdOptions() {
+		Option input = new Option("i", true, "数据文件路径"); input.setArgName("inputPath");
+		Option dims = new Option("d", true, "维度数量"); dims.setArgName("dimensionCount");
+		Option full = new Option("d", false, "全量加载");
+		Option output = new Option("o", true, "结果保存路径"); output.setArgName("outputPath");
+		return new Options().addOption(input).addOption(dims).addOption(full).addOption(output);
+	}
+	
+	private static void printHelp(Options options) {
+		new HelpFormatter().printHelp("java -cp SingleValueCube.jar shaochen.cube.pipe.LatticeEvaluator", options);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		//解析命令行参数
+		Options options = LatticeSampler.createCmdOptions();
+		CommandLine cmd = null;
+		try {
+			cmd = new BasicParser().parse(options, args);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			LatticeSampler.printHelp(options);
+			return;
+		}
+		String inputPath = cmd.getOptionValue("i");
+		int dimensionCount = Integer.parseInt(cmd.getOptionValue("d"));
+
+		//配置Spark上下文
+		StringBuilder appName = new StringBuilder("PipeScheduler").append(" -i " + inputPath);
+		SparkConf conf = new SparkConf().setAppName(appName.toString());
+		JavaSparkContext context = new JavaSparkContext(conf);
+
+		//统计搜索格的计算结果
+		double fraction = cmd.hasOption("f") ? 1.0 : LatticeSampler.calculateSampleFraction(context, inputPath);
+		Map<Integer, Long> map = LatticeSampler.estimateCuboidSize(context, inputPath, fraction, dimensionCount);
+		
+		//存储搜索格的统计结果
+		ObjectOutputStream out = null;
+		try {
+			out = new ObjectOutputStream(new FileOutputStream(cmd.getOptionValue("o")));
+			out.writeObject(map);
+		} finally {
+			out.close();
+		}
 	}
 	
 }
