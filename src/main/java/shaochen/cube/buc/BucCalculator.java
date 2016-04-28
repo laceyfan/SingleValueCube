92e6534ca8d907fe437cae84f16719c5534f633c
shishaochen <shishaochen_ha@sina.com>
2016-04-28 19:08:03 +0800

    完成算法还原。

--- src/main/java/shaochen/cube/buc/BucCalculator.java
+++ src/main/java/shaochen/cube/buc/BucCalculator.java
@@ -1,6 +1,9 @@
 package shaochen.cube.buc;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
 
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -11,10 +14,10 @@ import org.apache.commons.cli.ParseException;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.broadcast.Broadcast;
-import org.apache.spark.storage.StorageLevel;
 
 import scala.Tuple2;
 import shaochen.cube.plan.BinaryTree;
@@ -74,16 +77,16 @@ public class BucCalculator {
 		int parallelism = context.getConf().getInt("spark.default.parallelism", 20);
 		final Broadcast<Integer> bWidth = context.broadcast(dimensionCount);
 		final Broadcast<Integer> bFromMark = context.broadcast((int) Math.pow(2, dimensionCount) - 1);
-		JavaPairRDD<Member, Tuple2<Member, Long>> table = context.textFile(cmd.getOptionValue("i"), parallelism).mapToPair(new PairFunction<String, Member, Tuple2<Member, Long>>() {
+		JavaPairRDD<Member, Long> table = context.textFile(cmd.getOptionValue("i"), parallelism).mapToPair(new PairFunction<String, Member, Long>() {
 
 			private static final long serialVersionUID = 1L;
 
-			public Tuple2<Member, Tuple2<Member, Long>> call(String t) throws Exception {
+			public Tuple2<Member, Long> call(String t) throws Exception {
 				String[] fields = t.split("\\|");
 				int width = bWidth.value();
 				Member member = new Member(Arrays.copyOf(fields, width), bFromMark.value());
 				long quantity = Long.parseLong(fields[width]);
-				return new Tuple2<Member, Tuple2<Member, Long>>(member, new Tuple2<Member, Long>(member, quantity));
+				return new Tuple2<Member, Long>(member, quantity);
 			}
 
 		});
@@ -93,46 +96,64 @@ public class BucCalculator {
 		context.close();
 	}
 	
-	private static void preOrderTraverse(JavaSparkContext context, JavaPairRDD<Member, Tuple2<Member, Long>> input, BinaryTree<Cuboid> node, String outputDir) {
+	private static void preOrderTraverse(JavaSparkContext context, JavaPairRDD<Member, Long> input, BinaryTree<Cuboid> node, String outputDir) {
 		if (node == null) {
 			return;
 		}
 		
-		//计算当前格点的聚集结果
+		//重置维度值
 		final Broadcast<Integer> bToMark = context.broadcast(node.getValue().getMark());
-		JavaPairRDD<Member, Tuple2<Member, Long>> output = input.mapToPair(new PairFunction<Tuple2<Member, Tuple2<Member, Long>>, Member, Tuple2<Member, Long>>() {
+		JavaPairRDD<Member, Long> temp = input.mapToPair(new PairFunction<Tuple2<Member, Long>, Member, Long>() {
 
 			private static final long serialVersionUID = 1L;
 
-			public Tuple2<Member, Tuple2<Member, Long>> call(Tuple2<Member, Tuple2<Member, Long>> t) throws Exception {
-				Member key = t._2()._1().clone().reset(bToMark.value());
-				return new Tuple2<Member, Tuple2<Member, Long>>(key, t._2());
+			public Tuple2<Member, Long> call(Tuple2<Member, Long> t) throws Exception {
+				Member m = t._1().clone().reset(bToMark.value());
+				return new Tuple2<Member, Long>(m, t._2());
 			}
 			
-		}).persist(StorageLevel.MEMORY_AND_DISK());
-		
-		output.reduceByKey(new Function2<Tuple2<Member, Long>, Tuple2<Member, Long>, Tuple2<Member, Long>>() {
+		}).cache();
+		bToMark.destroy();
+
+		//获取无效分区
+		final List<Member> filtered = new LinkedList<Member>();
+		temp.reduceByKey(new Function2<Long, Long, Long>() {
 
 			private static final long serialVersionUID = 1L;
 
-			public Tuple2<Member, Long> call(Tuple2<Member, Long> v1, Tuple2<Member, Long> v2) throws Exception {
-				return new Tuple2<Member, Long>(v1._1(), v1._2() + v2._2());
+			public Long call(Long v1, Long v2) throws Exception {
+				return v1 + v2;
 			}
 			
-		}).mapToPair(new PairFunction<Tuple2<Member, Tuple2<Member, Long>>, Member, Long>() {
+		}).collect().forEach(new Consumer<Tuple2<Member, Long>>() {
+
+			public void accept(Tuple2<Member, Long> t) {
+				if (t._2() > 0L) {
+					filtered.add(t._1());
+				}
+			}
+			
+		});
+
+		//过滤无效分区
+		final Broadcast<List<Member>> bfiltered = context.broadcast(filtered);
+		JavaPairRDD<Member, Long> output = temp.filter(new Function<Tuple2<Member, Long>, Boolean>() {
 
 			private static final long serialVersionUID = 1L;
 
-			public Tuple2<Member, Long> call(Tuple2<Member, Tuple2<Member, Long>> t) throws Exception {
-				return new Tuple2<Member, Long>(t._1(), t._2()._2());
+			public Boolean call(Tuple2<Member, Long> t) throws Exception {
+				return bfiltered.value().equals(t._1());
 			}
 			
-		}).saveAsObjectFile(outputDir + node.getValue().getMark());
+		}).cache();
+		bfiltered.destroy();
+		temp.unpersist();
+		output.saveAsObjectFile(outputDir + node.getValue().getMark());
 
 		//执行其余格点
 		BucCalculator.preOrderTraverse(context, output, node.getLeft(), outputDir); //先执行子节点
-		BucCalculator.preOrderTraverse(context, input, node.getRight(), outputDir); //执行兄弟节点
 		output.unpersist();
+		BucCalculator.preOrderTraverse(context, input, node.getRight(), outputDir); //执行兄弟节点
 	}
 
 }
